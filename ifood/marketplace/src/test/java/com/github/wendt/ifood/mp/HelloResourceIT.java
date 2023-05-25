1eefc4f3db5ff0246f50b02c1b522c07abfee1c1
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-05-25 12:48:39 -0300

    feat: :art: first configuration

--- ifood/marketplace/src/test/java/com/github/wendt/ifood/mp/HelloResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.wendt.ifood.mp;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HelloResourceIT extends HelloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
